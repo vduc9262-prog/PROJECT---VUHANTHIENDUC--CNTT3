5fa532bb57aea2ed86f10f3a88da16f6835c744b
Vuduc2907 <vduc9262@gmail.com>
2026-04-07 18:35:05 +0700

    Update script.js

---
+++ DỰ ÁN HỌC TỪ VỰNG NGOẠI NGỮ/flash card/script.js
@@ -0,0 +1,52 @@
+const animal = [
+    {
+      id: 1,
+      word: "Cat",
+      meaning: "mèo",
+      status: "Not Learned",  
+    },
+    {
+      id: 2,
+      word: "Dog",
+      meaning: "Chó",
+      status: "Not Learned",  
+    },
+];
+
+let SaveData = () => {
+    localStorage.setItem("listAnimal", JSON.stringify(animal)) || [];
+}
+
+let loadData = () => {
+
+    let data = localStorage.getItem("listAnimal");
+    if (data) {
+        animal = JSON.parse(data);
+    }
+}
+
+
+loadData();
+
+
+
+let wordTableBody = document.getElementById("word-table-body");
+
+const renderAnimal = () => {
+   let html = ``;
+   animal.forEach((el) => {
+      html += `
+          <tr>
+              <td>${el.word}</td>
+              <td>${el.meaning}</td>
+              <td>${el.status}</td>
+         </tr>
+                         
+      `
+      wordTableBody.innerHTML = html 
+   });
+SaveData();
+
+
+};
+renderAnimal();
